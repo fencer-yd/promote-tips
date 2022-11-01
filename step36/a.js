234e4d8c5c290323c0765a3be9c346bb7ff14129
fencer <982039620@qq.com>
2022-11-01 19:42:02 +0800

    add 29 - 36

---
+++ step36/a.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: fencer 982039620@qq.com
+ * @Date: 2022-11-01 16:35:17
+ * @LastEditors: fencer 982039620@qq.com
+ * @LastEditTime: 2022-11-01 16:40:32
+ * @FilePath: /promote-tips/step36/a.js
+ * @Description: step36
+ */
+var start = Date.now();
+
+while (Date.now() - start < 2000) {}
+
+var p = document.createElement("p");
+p.innerHTML = "OK";
+document.body.appendChild(p);
