f212636722b37437d7aae034c3de95ceeb2e3df9
fencer <yangd@mshutech.com>
2023-01-01 17:38:17 +0800

    add deep clone

--- step48/index.js
+++ step48/index.js
@@ -2,6 +2,16 @@
  * @description 1.将一个树状结构的数组转成扁平化数组
  * @description 2.将一个扁平化数组转成树状结构数组
  */
+function deepClone(value) {
+  if (typeof value === 'object' && value !== null) {
+    const result = Array.isArray(value) ? [] : {};
+    for (const key in value) {
+      result[key] = deepClone(value[key])
+    }
+    return result
+  }
+  return value
+}
 
 const  data1 = [
   {
@@ -50,6 +60,7 @@ const result1 = data1.reduce(function (acc, cur) {
 }, []);
 
 console.log(result1);
+console.log(data1);
 
 const data2 = [
   { id: '1', name: '父节点1', parentId: undefined },
@@ -60,8 +71,8 @@ const data2 = [
   { id: '2-1', name: '子节点2-1', parentId: '2' }
 ];
 
-
-const result2 = data2.reduce(function (acc, cur, idx, arr) {
+// 此处需要做深度克隆处理，否则会改变data2本身
+const result2 = deepClone(data2).reduce(function (acc, cur, idx, arr) {
   // 检索当前元素的子元素; tips: 此时操作cur会改变arr本身
   cur.cildren = arr.filter(item => item.parentId === cur.id);
   // 判断是否为根元素
@@ -72,4 +83,4 @@ const result2 = data2.reduce(function (acc, cur, idx, arr) {
 }, [])
 
 console.log(JSON.stringify(result2, null, 2));
-
+console.log(data2);
